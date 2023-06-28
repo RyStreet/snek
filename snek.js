f14ac7b703f3704734c7aa67cc62723b8d5a73c7
RyStreet <ryanstreet122@gmail.com>
2023-06-28 14:12:07 -0500

    debugging

--- snek.js
+++ snek.js
@@ -3,7 +3,9 @@ let scoreDisplay = document.querySelector(".scoreDisplay")
 let up = document.querySelector(".up")
 let bottom = document.querySelector(".bottom")
 let left = document.querySelector(".left")
-let right = document.querySelector("right")
+let right = document.querySelector(".right")
+let popup = document.querySelector(".popup")
+let playAgain = document.querySelector(".playAgain")
 
 let width = 10
 let currentIndex = 0
@@ -32,6 +34,7 @@ function createBoard () {
 }
 
 function startGame(){
+    console.log("startgame")
     let squares = document.querySelectorAll(".grid div");
     randomApple(squares)
     direction = 1
@@ -44,5 +47,82 @@ function startGame(){
 }
 
 function moveOutcome() {
-    let squares = document.querySelectorAll
+    let squares = document.querySelectorAll(".grid div")
+    if(checkHit(squares)){
+        alert("Game Over!")
+        popup.style.display = "flex"
+        return clearInterval(interval)
+    } else{
+        moveSnake(squares)
+    }
+}
+
+function moveSnake(squares){
+    let tail = currentSnake.pop()
+    squares[tail].classList.remove("snake");
+    currentSnake.unshift(currentSnake[0] + direction);
+    console.log(currentSnake[0] + direction)
+    eatApple(squares, tail);
+    squares[currentSnake[0]].classList.add("snake")
+}
+
+function checkHit(squares) {
+    if (
+        (currentSnake[0] + width >= width * width && direction === width) ||
+        (currentSnake[0] % width === width -1 && direction === 1 ) ||
+        (currentSnake[0] % width === 0 && direction === -1 ) ||
+        (currentSnake[0] - width <= 0 && direction === -width) ||
+        squares[currentSnake[0] + direction].classList.contains("snake")
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function randomApple(squares) {
+    do{
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains("snake"));
+    squares[appleIndex].classList.add("apple")
+}
+
+function eatApple(squares, tail) {
+    if(squares[currentSnake[0]].classList.contains("apple")){
+        squares[currentSnake[0]].classList.remove("apple")
+        squares[tail].classList.add("snake")
+        currentSnake.push(tail)
+        randomApple(squares)
+        score++
+        scoreDisplay.textContent = score
+        clearInterval(interval)
+        intervalTime = intervalTime * speed
+        interval = setInterval(moveOutcome, intervalTime)
+    }
+}
+
+
+function control(e){
+    if(e.keycode === 39){
+        direction = 1
+    } else if(e.keycode === 38){
+        direction = -width
+    } else if(e.keycode === 37){
+        direction = -1
+    } else if(e.keycode === 40){
+        direction = +width
+    }
+
+}
+
+up.addEventListener("click", () => (direction = -width))
+bottom.addEventListener("click", () => (direction = +width))
+left.addEventListener("click", () => (direction = -1))
+right.addEventListener("click", () => (direction = 1))
+
+function replay(){
+    grid.innerHTML = ""
+    createBoard()
+    startGame()
+    popup.style.display = "none"
 }
